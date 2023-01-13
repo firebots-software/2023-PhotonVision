02952ea203d1b148b30c885ba2c4e263a9a16ed6
YajwinGrover <yajwin.grover@gmail.com>
2023-01-12 21:18:26 -0800

    Testing PhotonUtils

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,10 +5,16 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.DistanceFromTag;
+import frc.robot.subsystems.PhotonVision;
 
 public class Robot extends TimedRobot {
+  PhotonVision pv = new PhotonVision();
+  DistanceFromTag dTag = new DistanceFromTag();
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
@@ -16,11 +22,14 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+    //distanceFromTag.execute();
+
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+    System.out.println(dTag.dist());
   }
 
   @Override
@@ -70,4 +79,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testExit() {}
+
+  
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,15 +6,26 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.DistanceFromTag;
+import frc.robot.subsystems.PhotonVision;
 
 public class RobotContainer {
+ 
+ 
   public RobotContainer() {
+    
+    //pv.setDefaultCommand(new DistanceFromTag());
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings(){
+     
+  } 
+
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
+
+
 }
