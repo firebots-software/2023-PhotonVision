7e36b14de032dfe49f625555c0cedefb74689b53
Rawpower9 <ritviksetty@gmail.com>
2023-01-26 19:38:07 -0800

    Finished Tuning PID lj

--- src/main/java/frc/robot/commands/SwervePID.java
+++ src/main/java/frc/robot/commands/SwervePID.java
@@ -6,9 +6,13 @@ import frc.robot.subsystems.SwerveModule;
 import frc.robot.Constants.DriveConstants;;
 public class SwervePID extends CommandBase{
     PIDController pid;
-    SwerveModule module;
+    SwerveModule modulefl;
+    SwerveModule modulefr;
+    SwerveModule modulebl;
+    SwerveModule modulebr;
+
     public SwervePID(){
-        module = new SwerveModule(
+        modulefl = new SwerveModule(
             DriveConstants.kFrontLeftDriveMotorPort,
             DriveConstants.kFrontLeftTurningMotorPort,
             DriveConstants.kFrontLeftDriveAbsoluteEncoderPort,
@@ -16,23 +20,56 @@ public class SwervePID extends CommandBase{
             DriveConstants.kFrontLeftTurningEncoderReversed,
             DriveConstants.kFrontLeftDriveAbsoluteEncoderOffsetRad
             );    
-        pid = new PIDController(0.0001,0,0);
+        modulefr = new SwerveModule(
+            DriveConstants.kFrontRightDriveMotorPort,
+            DriveConstants.kFrontRightTurningMotorPort,
+            DriveConstants.kFrontRightDriveAbsoluteEncoderPort,
+            DriveConstants.kFrontRightDriveEncoderReversed,
+            DriveConstants.kFrontRightTurningEncoderReversed,
+            DriveConstants.kFrontRightDriveAbsoluteEncoderOffsetRad);
+        modulebl = new SwerveModule(
+            DriveConstants.kBackLeftDriveMotorPort,
+            DriveConstants.kBackLeftTurningMotorPort,
+            DriveConstants.kBackLeftDriveAbsoluteEncoderPort,
+            DriveConstants.kBackLeftDriveEncoderReversed,
+            DriveConstants.kBackLeftTurningEncoderReversed,
+            DriveConstants.kBackLeftDriveAbsoluteEncoderOffsetRad
+        );
+        modulebr = new SwerveModule(
+            DriveConstants.kBackRightDriveMotorPort,
+            DriveConstants.kBackRightTurningMotorPort,
+            DriveConstants.kBackRightDriveAbsoluteEncoderPort,
+            DriveConstants.kBackRightDriveEncoderReversed,
+            DriveConstants.kBackRightTurningEncoderReversed,
+            DriveConstants.kBackRightDriveAbsoluteEncoderOffsetRad
+        );
+        //fl: 0.000053, 0.000069, 0
+        //fr: 0.000063, 0.000069,0
+        //bl: 0.000053, 0.000069, 0
+        //br: 0.000073, 0.000069,0
+        pid = new PIDController(0.000073,0.000069,0);
         pid.setTolerance(0.5);
 
     }
 
     @Override
     public void initialize(){
-        pid.setSetpoint(module.getPosition() + 5*2048);
+        //pid.setSetpoint(modulefl.getPosition() + 1*2048*6.75);
+        //pid.setSetpoint(modulefr.getPosition()+ 1*2048*6.75);
+        // pid.setSetpoint(modulebl.getPosition()+ 1*2048*6.75);
+        pid.setSetpoint(modulebr.getPosition()+ 1*2048*6.75);
+
     }
 
 
     @Override
     public void execute(){
-        double speed = pid.calculate(module.getPosition());
-        module.setMotor(speed);
-        System.out.println(speed);
-
+        //double speed = pid.calculate(module.getPosition());
+        //module.setMotor(speed);
+        //System.out.println(speed);
+        double speed1 = pid.calculate(modulebr.getPosition());
+        modulebr.setMotor(speed1);
+        System.out.println(speed1);
     }
 
     @Override
